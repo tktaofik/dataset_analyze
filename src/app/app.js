bda0c6d4f35a6160a1cb5eac647e2af6ff4a9100
Taofiki Yussuff <tktaofik@gmail.com>
2017-05-07 23:22:55 -0400

    Added layout for app

--- src/app/app.js
+++ src/app/app.js
@@ -1,31 +1,69 @@
-import React, {Component} from 'react';
-import { Button, Menu, Dropdown, Icon } from 'antd';
+import React from 'react';
 import './app.css';
+import {Layout, Menu, Breadcrumb, Icon} from 'antd';
+const {Header, Content, Footer, Sider} = Layout;
+const SubMenu = Menu.SubMenu;
 
-function handleMenuClick(e) {
-    console.log('click', e);
-}
 
-const menu = (
-    <Menu onClick={handleMenuClick}>
-        <Menu.Item key="1">1st item</Menu.Item>
-        <Menu.Item key="2">2nd item</Menu.Item>
-        <Menu.Item key="3">3rd item</Menu.Item>
-    </Menu>
-);
+class App extends React.Component {
+    state = {
+        collapsed: false,
+        mode: 'inline',
+    };
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({
+            collapsed,
+            mode: collapsed ? 'vertical' : 'inline',
+        });
+    };
 
-class App extends Component {
     render() {
         return (
-            <div>
-                <Button type="primary">primary</Button>
-                <Button>secondary</Button>
-                <Dropdown overlay={menu}>
-                    <Button>
-                        more <Icon type="down" />
-                    </Button>
-                </Dropdown>
-            </div>
+            <Layout>
+                <Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['6']}>
+                        <SubMenu
+                            key="sub1"
+                            title={<span><Icon type="user"/><span className="nav-text">User</span></span>}>
+                            <Menu.Item key="1">Tom</Menu.Item>
+                            <Menu.Item key="2">Bill</Menu.Item>
+                            <Menu.Item key="3">Alex</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            title={<span><Icon type="team"/><span className="nav-text">Team</span></span>}>
+                            <Menu.Item key="4">Team 1</Menu.Item>
+                            <Menu.Item key="5">Team 2</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="6">
+              <span>
+                <Icon type="file"/>
+                <span className="nav-text">File</span>
+              </span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{background: '#fff', padding: 0}}/>
+                    <Content style={{margin: '0 16px'}}>
+                        <Breadcrumb style={{margin: '12px 0'}}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        Qlik Analyze ©2017 Created by TK
+                    </Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
