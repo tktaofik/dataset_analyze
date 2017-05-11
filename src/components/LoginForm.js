386a60b70d779b1d3dc88a3dd294d5b3afa73d04
Taofiki Yussuff <tktaofik@gmail.com>
2017-05-11 06:46:22 -0400

    Login form clean up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,75 +1,66 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  withRouter
-} from 'react-router-dom'
-
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-
+import {Redirect} from 'react-router-dom'
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
 import '../styles/index.css';
 
 const FormItem = Form.Item;
 
-class NormalLoginForm extends React.Component {
-  state = {
-    redirectToReferrer: false
-  };
+class LoginForm extends React.Component {
+    state = {
+        redirectToReferrer: false
+    };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-    this.setState({ redirectToReferrer: true})
-  }
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    const { redirectToReferrer } = this.state;
-    const { insights } = { insights: { pathname: '/insights' } }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+        this.setState({redirectToReferrer: true})
+    };
 
-    if(redirectToReferrer) {
-      return (
-        <Redirect to={ insights }/>
-      );
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        const {redirectToReferrer} = this.state;
+        const {insights} = {insights: {pathname: '/insights'}};
+
+        if (redirectToReferrer) {
+            return (
+                <Redirect to={ insights }/>
+            );
+        }
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{required: true, message: 'Please input your username!'}],
+                    })(
+                        <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{required: true, message: 'Please input your Password!'}],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+                               placeholder="Password"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(
+                        <Checkbox>Remember me</Checkbox>
+                    )}
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                </FormItem>
+            </Form>
+        );
     }
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </FormItem>
-      </Form>
-    );
-  }
 }
 
-const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
-
-export default WrappedNormalLoginForm;
+export default Form.create()(LoginForm);
