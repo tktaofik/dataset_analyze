e81c2e26a5abec7f8ae23adbc640bd411666ff7c
Jasonjys <jasonjys0511@gmail.com>
2017-06-20 14:55:41 -0400

    remove middleware stuff clean up API calls

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,21 +1,19 @@
-// import {createStore, applyMiddleware} from 'redux';
 import {createStore, applyMiddleware} from 'redux';
 import {rootReducer} from '../reducers/index';
 import {loadState, saveState} from './localstorage';
 import thunkMiddleware from 'redux-thunk';
 import throttle from 'lodash/throttle';
-import api from '../middleware/api'
 
-const persistedState = loadState();
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, api)(createStore);
+//const persistedState = loadState();
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 const store = createStoreWithMiddleware(
     rootReducer,
-    persistedState,
+    //persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-store.subscribe(throttle(() => {
-    saveState(store.getState());
-}));
+// store.subscribe(throttle(() => {
+//     saveState(store.getState());
+// }));
 
 export default store;
