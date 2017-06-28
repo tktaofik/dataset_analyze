ea7dc670d88772448fe74acfd0f00fefb9fdaecb
Taofiki Yussuff <tktaofik@gmail.com>
2017-06-28 19:36:25 -0400

    completed CRUD test cases for dataset dao

--- api/file/dao.go
+++ api/file/dao.go
@@ -68,10 +68,51 @@ func (d Dao) SaveDataset(dataset Dataset) (Dataset, error) {
 
 	c := s.DB(db.Name()).C(collection)
 
-	err = c.Insert(dataset)
+	if err = c.Insert(dataset); err != nil {
+		return dataset, errors.New("There was an error trying to insert a new dataset to the DB.")
+	}
+
+	return dataset, err
+}
+
+func (d Dao) UpdateDataset(dataset Dataset) (Dataset, error) {
+	db := config.DB{}
+	s, err := db.DoDial()
+	if err != nil {
+		return dataset, errors.New("There was an error trying to connect to the DB.")
+	}
+
+	defer s.Close()
+
+	c := s.DB(db.Name()).C(collection)
+
+	if err := c.Update(bson.M{"_id": dataset.Id},dataset); err != nil {
+		return dataset, errors.New("There was an error trying to update dataset in the DB.")
+	}
 
 	if err != nil {
-		return dataset, errors.New("There was an error trying to insert a new dataset doc to the DB.")
+		return dataset, errors.New("There was an error trying to update  dataset in the DB.")
+	}
+
+	return dataset, err
+}
+
+func (d Dao) DeleteDataset(dataset Dataset) (Dataset, error) {
+	db := config.DB{}
+	s, err := db.DoDial()
+	if err != nil {
+		return dataset, errors.New("There was an error trying to connect to the DB.")
+	}
+
+	defer s.Close()
+
+	c := s.DB(db.Name()).C(collection)
+	if err := c.Remove(bson.M{"_id": dataset.Id}); err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		return dataset, errors.New("There was an error trying to remove dataset in the DB.")
 	}
 
 	return dataset, err
--- api/file/dao_test.go
+++ api/file/dao_test.go
@@ -47,7 +47,42 @@ func TestGetDatasetById(t *testing.T) {
 	result, err := dao.GetDatasetById(bson.ObjectId.Hex(dataset.Id))
 
 	if err != nil {
-		t.Error("Failed to get DatasetById")
+		t.Error("Failed to get Dataset by Id")
+	}
+
+	assert.IsType(t, Dataset{}, result, "Expected data type Dataset")
+}
+
+func TestUpdateDataset(t *testing.T) {
+	expected_res := new(Dataset)
+
+	if err := json.Unmarshal([]byte(sampleDataset()), &expected_res); err != nil {
+		t.Error("Unable to marshal TestSaveFileAsDataset expected JSON response'")
+	}
+
+	result, err := dao.UpdateDataset(*dataset)
+
+	if err != nil {
+		t.Error("Failed to get Update dataset")
+	}
+
+	assert.IsType(t, Dataset{}, result, "Expected data type Dataset")
+	assert.Equal(t, expected_res.Attributes.Name, result.Attributes.Name)
+	assert.Equal(t, expected_res.Attributes.Size, result.Attributes.Size)
+	assert.Equal(t, expected_res.Attributes.RawData, result.Attributes.RawData)
+	assert.Equal(t, expected_res.Attributes.Tables, result.Attributes.Tables)
+	assert.Equal(t, expected_res.Attributes.Link, result.Attributes.Link)
+	assert.Equal(t, expected_res.User.Id, result.User.Id)
+	assert.Equal(t, expected_res.User.Link, result.User.Link)
+	assert.NotEmpty(t, result.Id)
+	assert.NotEmpty(t, result.CreatedAt)
+}
+
+func TestDeleteDataset(t *testing.T) {
+	result, err := dao.DeleteDataset(*dataset)
+
+	if err != nil {
+		t.Error("Failed to get Delete Dataset")
 	}
 
 	assert.IsType(t, Dataset{}, result, "Expected data type Dataset")
