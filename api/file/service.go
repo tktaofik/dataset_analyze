b30150cce11e2a4d3b9ef17ed243a04b4f55e7c2
Taofiki Yussuff <tktaofik@gmail.com>
2017-07-18 13:05:11 -0400

    accessing dataset data structures properly now

--- api/file/service.go
+++ api/file/service.go
@@ -10,42 +10,38 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type ServiceObj interface {
-	SaveFileAsDataset(d Dataset) (Dataset, error)
-	GetDatasets() (Datasets, error)
-}
-
 type Dataset struct {
-	Id        bson.ObjectId        `json:"id,omitempty" bson:"_id,omitempty" `
-	CreatedAt time.Time             `json:"created_at"`
-	Type      string                `json:"type"`
+	Id         bson.ObjectId        `json:"id,omitempty" bson:"_id,omitempty" `
+	CreatedAt  time.Time             `json:"created_at"`
+	Type       string                `json:"type"`
 	Attributes struct {
-		Name    string `json:"name,omitempty"`
-		Size    int `json:"size,omitempty"`
-		RawData string `json:"raw_data,omitempty"`
-		Tables  Tables `json:"tables,omitempty"`
+			   Name    string `json:"name,omitempty"`
+			   Size    int `json:"size,omitempty"`
+			   RawData string `json:"raw_data,omitempty"`
+			   Tables  Tables `json:"tables,omitempty"`
 
-		Link string `json:"link,omitempty"`
-	} `json:"attributes"`
-	User struct {
-		Id   string `json:"id,omitempty" bson:"_id,omitempty" `
-		Link string `json:"link,omitempty"`
-	} `json:"user"`
-	Link string                         `json:"link,omitempty"`
+			   Link    string `json:"link,omitempty"`
+		   } `json:"attributes"`
+	User       struct {
+			   Id   string `json:"id,omitempty" bson:"_id,omitempty" `
+			   Link string `json:"link,omitempty"`
+		   } `json:"user"`
+	Link       string                         `json:"link,omitempty"`
 }
 
 type Datasets []Dataset
 
 type Table struct {
-	Rows []map[string]interface{} `json:"rows,omitempty"`
-	Columns []struct {
-		Name string `json:"name,omitempty"`
-		Data []interface{} `json:"columnName,omitempty"`
-	} `json:"columns,omitempty"`
+	Rows      []map[string]interface{} `json:"rows,omitempty"`
+	Columns   Columns `json:"columns,omitempty"`
 	TableName string `json:"tableName,omitempty"`
 }
 
 type Tables []Table
+
+type Column map[string]interface{}
+
+type Columns []Column
 
 type Service struct {
 	Dao
@@ -53,15 +49,16 @@ type Service struct {
 
 func (fs Service) DatasetTableColumns(d Dataset) (Dataset) {
 	tables := reflect.ValueOf(d.Attributes.Tables)
-
 	for i := 0; i < tables.Len(); i++ {
 		table := reflect.Value(tables.Index(i)).Interface().(Table)
-		keys := reflect.ValueOf(table.Rows[0]).MapKeys()
+		tableRowKeys := reflect.ValueOf(table.Rows[0]).MapKeys()
 
-		fmt.Println(keys)
-		//for _, row := range table.Rows {
-		//	fmt.Println(i, row)
-		//}
+		for _, row := range table.Rows {
+			for _, key := range tableRowKeys {
+				key := reflect.Value(key).Interface().(string)
+				fmt.Printf("key: %s value: %s \n", key, row[key])
+			}
+		}
 	}
 	return d
 }
